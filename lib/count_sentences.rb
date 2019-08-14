d36038f074fddc7678c85ab57c9e4947397d8edb
Paul Beckwith <303dog@gmail.com>
2019-08-14 18:26:58 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,52 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")      
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
-
+  def count_sentences(string)
+    self.split.length
   end
+  #binding.pry
+  
 end
+
+
+
+
+
+
+
+#class String
+
+ # def sentence?
+  #  self.end_with?(".")
+  #end
+ 
+ # def question?
+  #  if self.end_with?("?")
+
+      
+  #end
+
+  #def exclamation?
+   # if self.end_with? == ("!")
+    #  puts "true"
+  #end
+
+  #def count_sentences
+   # self.ends_with? == String.split
+    #  self.length
+  #end
+  #end
+  #end
+  
+#end
